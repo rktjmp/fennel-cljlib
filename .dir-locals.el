4288f1f60c7445dd42e2e93b3d5cf5700d3dcec8
Andrey Orst <andreyorst@gmail.com>
2020-10-26 22:51:39 +0300

    fix(misc): fix indentation rules

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((fennel-mode . ((eval . (put 'test 'fennel-indent-function 'defun))
+((fennel-mode . ((eval . (put 'deftest 'fennel-indent-function 'defun))
+                 (eval . (put 'testing 'fennel-indent-function 'defun))
                  (eval . (put 'when-some 'fennel-indent-function 1))
                  (eval . (put 'if-some 'fennel-indent-function 1))
                  (eval . (put 'when-let 'fennel-indent-function 1))
